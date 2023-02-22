6bffbe5cd4309ac4d6f0ac38c169ece92314085f
5G2 <ivanng1997327@gmail.com>
2023-02-22 15:36:43 +0800

    update data field

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,51 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = ()=> {
-   
-    return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <Link className="navbar-brand " to="/">Agriculture simulator</Link>
-          <button className="navbar-toggler" type="button"  >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarScroll">
-            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand " to="/">
+          Agriculture simulator
+        </Link>
+        <button className="navbar-toggler" type="button">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarScroll">
+          <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
             <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/information">Information</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/home">Control Panel</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Tutorial</Link>
-              </li>
-         
-            </ul>
-          
-          </div>
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/information"
+              >
+                Information
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/controlPanel"
+              >
+                Control Panel
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Tutorial
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  );
 };
 
-
-
-export default (Navbar);
+export default Navbar;
--- src/containers/Information.js
+++ src/containers/Information.js
@@ -1,129 +1,136 @@
-import React, {useState,useEffect}from 'react';
+import React, { useState, useEffect } from "react";
 import { Link, Navigate } from "react-router-dom";
 // import getTemperature from '../action/update/InformationUpdate';
 
+const Information = () => {
+  const [temperature, setTemperature] = useState();
+  const [soid_humidity, setsoid_humidity] = useState();
+  const [air_humidity, setair_humidity] = useState();
+  const [light, setlight] = useState();
 
-
-const Information =() =>{
-
-    const [temperature, setTemperature] = useState();
-    const [humidity, setHumidity] = useState();
-    const [light, setLight] = useState();
-    const [co2, setCo2] = useState();
-
-
-    async function getTemperature() {
-        const response = await fetch(
-            `https://api.thingspeak.com/channels/2034242/fields/1.json?results=1`,{
-                method:'GET',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },})
-        let actualData = await response.json();
-        setTemperature(actualData.feeds[0].field1) 
+  async function getTemperature() {
+    const response = await fetch(
+      `https://api.thingspeak.com/channels/2041520/fields/1.json?results=1`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
       }
-    
-      async function getLight() {
-        const response = await fetch(
-            `https://api.thingspeak.com/channels/2034242/fields/1.json?results=1`,{
-                method:'GET',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },})
-        let actualData = await response.json();
-        setHumidity(actualData.feeds[0].field1) 
-      }
-      async function getCo2() {
-        const response = await fetch(
-            `https://api.thingspeak.com/channels/2034242/fields/1.json?results=1`,{
-                method:'GET',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },})
-        let actualData = await response.json();
-        setLight(actualData.feeds[0].field1) 
-      }
-      async function getHumidity() {
-        const response = await fetch(
-            `https://api.thingspeak.com/channels/2034242/fields/1.json?results=1`,{
-                method:'GET',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },})
-        let actualData = await response.json();
-        setCo2(actualData.feeds[0].field1) 
-      }
-      
-    useEffect(() => {
-        getTemperature();
-        getLight();
-        getCo2();
-        getHumidity();
-      }, [])
+    );
+    let actualData = await response.json();
+    setTemperature(actualData.feeds[0].field1);
+    console.log("actualData: ");
+    console.log(actualData.feeds[0].field1);
+  }
 
-      
-      function update(){
-        // setTemperature(getTemperature());
-        // console.log(getTemperature())
+  async function getair_humidity() {
+    const response = await fetch(
+      `https://api.thingspeak.com/channels/2041520/fields/2.json?results=1`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
       }
+    );
+    let actualData = await response.json();
+    setsoid_humidity(actualData.feeds[0].field2);
+  }
+  async function getlight() {
+    const response = await fetch(
+      `https://api.thingspeak.com/channels/2041520/fields/3.json?results=1`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let actualData = await response.json();
+    setair_humidity(actualData.feeds[0].field3);
+  }
+  async function getsoid_humidity() {
+    const response = await fetch(
+      `https://api.thingspeak.com/channels/2041520/fields/4.json?results=1`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let actualData = await response.json();
+    setlight(actualData.feeds[0].field4);
+  }
 
-  
-    return (
+  useEffect(() => {
+    getTemperature();
+    getair_humidity();
+    getlight();
+    getsoid_humidity();
+  }, []);
 
-        <div className='container mt-5'>
-        <h1>Live Data!</h1>
-        
-            <div className='form-group'>
-                <p>Temperature:</p>
-                <input
-                    className='form-control'
-                    type='temperature'
-                    name='temperature'
-                    value={temperature+ " °C"}
-                    disabled
-                />
-            </div>
-            <br/>
-            <div className='form-group'>
-                <p>Humidity:</p>
-                <input
-                    className='form-control'
-                    type='humidity'
-                    name='humidity'
-                    value={humidity +" %"}
-                    disabled
-                />
-            </div>
-            <br/>
-            <div className='form-group'>
-                <p>Light Intensity:</p>
-                <input
-                    className='form-control'
-                    type='light'
-                    name='light'
-                    value={light +" LUX"}
-                    disabled
-                />
-            </div>
-            <br/>
-            <div className='form-group'>
-                <p>Co2</p>
-                <input
-                    className='form-control'
-                    type='co2'
-                    name='co2'
-                    value={co2 +" PPM"}
-                    disabled
-                />
-            </div>
-            <br/>
-            <button onClick={update()}> update</button>
+  function update() {
+    // setTemperature(getTemperature());
+    // console.log(getTemperature())
+  }
+
+  return (
+    <div className="container mt-5">
+      <h1>Live Data!</h1>
+
+      <div className="form-group">
+        <p>Temperature:</p>
+        <input
+          className="form-control"
+          type="temperature"
+          name="temperature"
+          value={temperature + " °C"}
+          disabled
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <p>Soid Humidity:</p>
+        <input
+          className="form-control"
+          type="soid_humidity"
+          name="soid_humidity"
+          value={soid_humidity + " %"}
+          disabled
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <p>Air Humidity:</p>
+        <input
+          className="form-control"
+          type="air_humidity"
+          name="air_humidity"
+          value={air_humidity + " %"}
+          disabled
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <p>Light</p>
+        <input
+          className="form-control"
+          type="light"
+          name="light"
+          value={light + " LUX"}
+          disabled
+        />
+      </div>
+      <br />
+      <button onClick={update()}> update</button>
     </div>
-    )
+  );
 };
 
 export default Information;
